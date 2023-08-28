99d12d2da192679a31fd3e40fd853dd66e672cd7
AjithAkar <ajithkumar8105@gmail.com>
2023-08-28 10:30:37 +0530

    logger added and check properties

--- client/src/main/java/com/example/clint/PropertiesConfiguration.java
+++ client/src/main/java/com/example/clint/PropertiesConfiguration.java
@@ -1,14 +1,26 @@
 package com.example.clint;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties("spring.test")
 @Configuration
 @RefreshScope
+
 public class PropertiesConfiguration {
+    @Value("${spring.test.value}")
     private String value;
+    @Value("${spring.profiles.active}")
+    private String profiles;
+
+    public String getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(String profiles) {
+        this.profiles = profiles;
+    }
 
     public String getValue() {
         return value;
@@ -17,4 +29,5 @@ public class PropertiesConfiguration {
     public void setValue(String value) {
         this.value = value;
     }
+
 }
