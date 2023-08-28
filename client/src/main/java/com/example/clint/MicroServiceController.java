f39217c3e553f9d3c2ac60d908a77d4c2df5b4d2
AjithAkar <ajithkumar8105@gmail.com>
2023-08-28 10:37:01 +0530

    logger added and check properties

--- client/src/main/java/com/example/clint/MicroServiceController.java
+++ client/src/main/java/com/example/clint/MicroServiceController.java
@@ -1,16 +1,21 @@
 package com.example.clint;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class MicroServiceController {
-
+ Logger log = Logger.getLogger("MicroServiceController");
     @Autowired
    private PropertiesConfiguration configuration;
 
     @GetMapping("/endpoint")
     public String retrieveLimits(){
+
+ log.info("properties :" + configuration.getProfiles());
         return configuration.getValue();
     }
 }
